b5f944debcaa86050f92e96a9eedf09db74893a5
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-25 18:54:38 -0400

    completed assignment

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,25 @@
 class Reader < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+
+    def subscribe(magazine, price)
+        # takes a magazine (an instance of the Magazine class) and a price (integer) as arguments, and creates a new subscription in the database associated with the magazine and the reader
+        Subscription.create(price: price, reader_id: self.id, magazine_id: magazine.id)
+    end
+
+    def total_subscription_price
+        # Returns the total price for all the reader's subscriptions
+        self.subscriptions.pluck(:price).sum
+    end
+
+    def cancel_subscription(magazine)
+        # takes a magazine instance and removes the subscription for this reader
+        # you will have to delete a row from the subscriptions table to get this to work!
+        self.subscriptions.where("magazine_id == ?", magazine.id).destroy_all
+    end
+
+
+
+
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,18 @@
 class Subscription < ActiveRecord::Base
-  
+    belongs_to :magazine
+    belongs_to :reader
+
+    def print_details
+        # puts a string to the terminal to display the details of the subscription
+        # the string should be formatted like this: {reader name} subscribed to {magazine title} for ${subscription price}
+        reader = self.reader.name
+        title = self.magazine.title
+        price = self.price
+        puts "#{reader} subscribed to #{title} for #{price}"
+    end
+
+
+    
+
+
 end
